fa8162ce0697422383196be445285d1dbda47ac1
Andreas Humenberger <ahumenberger@gmail.com>
2018-01-30 17:35:51 +0100

    Improve translation from sym to poly

--- src/ideals.jl
+++ src/ideals.jl
@@ -2,6 +2,8 @@
 import Singular.PolynomialRing
 import Singular.QQ
 
+using TimerOutputs
+
 #-------------------------------------------------------------------------------
 
 funcname(x) = PyCall.PyObject(x)[:func][:__name__]
@@ -29,30 +31,23 @@ val_map = Dict(
             #    "BooleanFalse"     => :false
 )
 
-map_fn(fn_map, key)   = get(fn_map, key, key)
-map_var(var_map, key) = get(var_map, key, key)
+# map_fn(fn_map, key)   = get(fn_map, key, key)
+# map_var(var_map, key) = get(var_map, key, key)
 
-function replace_sympy(ex; values=Dict(), fns=Dict())
-    fns_map = merge(fn_map, fns)
-    vals_map = merge(val_map, values)
-    
+function replace_sympy(ex, vars::Dict)
     fn = funcname(ex)
     
     if fn == "Symbol"
-        # println(typeof(ex))
-        return map_var(vals_map, ex)
-    elseif fn == "Function"
-        return Symbol("v_$(ex)")
+        return get(vars, string(ex), ex)
     elseif fn in ["Integer" , "Float"]
         return N(ex)
     elseif fn == "Rational"
         return QQ(convert(Int, numer(ex)))//QQ(convert(Int, denom(ex)))
-    elseif haskey(vals_map, fn)
-        return vals_map[fn]
+    elseif haskey(val_map, fn)
+        return val_map[fn]
     end
-
     error("should not be visited")
-    Expr(:call, map_fn(fns_map, fn), [replace_sympy(a, values=values) for a in args(ex)]...)
+    # Expr(:call, map_fn(fns_map, fn), [replace_sympy(a, values=values) for a in args(ex)]...)
 end
 
 @syms x y
@@ -60,48 +55,54 @@ const ADD = func(x+y)
 const MUL = func(x*y)
 const POW = func(x^y)
 
-function sym2spoly(b::Array{Sym, 1}; vars = Sym[])
-    fvars = union(free_symbols.(b)..., vars)
+function sym2spoly(b::Array{Sym, 1})
+    fvars = union(free_symbols.(b)...)
     svars = string.(fvars)
     R, pvars = PolynomialRing(QQ, svars)
-    dict = Dict(zip(fvars, pvars))
+    dict = Dict(zip(string.(fvars), pvars))
     println("+++++ replace: ", b)
-    basis = @time [shallow_replace(p, values = dict) for p in b]
+    basis = @time [shallow_replace(p, dict) for p in b]
     println("+++++ eval")
     basis = @time eval.(basis)
     return R, basis, dict
 end
 
-function shallow_replace(expr::Sym; values = Dict())
-    fn = func(expr)
+# const to = TimerOutput()
+
+function shallow_replace(expr::Sym, vars::Dict)
+    fn = funcname(expr)
     # println("Function: ", fn==ADD)
-    a = [shallow_replace(arg, values=values) for arg in args(expr)]
+    a = [shallow_replace(arg, vars) for arg in args(expr)]
     # println("Start: ", expr)
-    println("Result: ", a)
-    println("Result: ", typeof.(a))
-    println("Map: ", values)
-    if fn == ADD
+    if fn == "Add"
         return sum(a)
-    elseif fn == MUL
+    elseif fn == "Mul"
         return prod(a)
-    elseif fn == POW
+    elseif fn == "Pow"
         return a[1]^a[2]
     end
-    return replace_sympy(expr, values=values)
+    return replace_sympy(expr, vars)
 end
 
-function sym2spoly(p::Sym; vars = Sym[])
-    sym2spoly([p], vars=vars)[1]
-end
+# function sym2spoly(p::Sym; vars = Sym[])
+#     sym2spoly([p], vars=vars)[1]
+# end
 
 function sym2spoly(poly::Sym, varmap::Dict)
-    eval(shallow_replace(poly, values = varmap))
+    # fns_map = merge(fn_map, fns)
+    # vals_map = merge(val_map, varmap)
+    eval(shallow_replace(poly, varmap))
 end
 
 #-------------------------------------------------------------------------------
 
-function Ideal(basis::Array{Sym, 1}; vars = Sym[])
-    R, sbasis, varmap = sym2spoly(basis, vars=vars)
+# function Ideal(basis::Array{Sym, 1}; vars = Sym[])
+#     R, sbasis, varmap = sym2spoly(basis, vars)
+#     return Singular.Ideal(R, sbasis...), varmap
+# end
+
+function Ideal(basis::Array{Sym, 1})
+    R, sbasis, varmap = sym2spoly(basis)
     return Singular.Ideal(R, sbasis...), varmap
 end
 
@@ -109,6 +110,6 @@ groebner(ideal::Singular.sideal) = std(ideal)
 
 function eliminate(basis::Array{Sym, 1}, elim::Array{Sym, 1})
     ideal, varmap = Ideal(basis)
-    vars = [varmap[v] for v in elim]
+    vars = [varmap[string(v)] for v in elim]
     return Singular.eliminate(ideal, prod(vars))
 end
