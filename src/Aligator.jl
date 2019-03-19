df8ef5bc74d72a6383996705e2e1bcad6bbfe0a8
Andreas Humenberger <ahumenberger@gmail.com>
2019-03-19 15:12:24 +0100

    First steps towards using new rec solver

--- src/Aligator.jl
+++ src/Aligator.jl
@@ -7,6 +7,9 @@ using PyCall
 using SymPy
 using Singular
 using ContinuedFractions
+using MacroTools
+using Recurrences
+using SymEngine
 
 const AppliedUndef = PyCall.PyNULL()
 
@@ -15,27 +18,52 @@ include("closedform.jl")
 include("recurrence.jl")
 include("loop.jl")
 include("invariants.jl")
-include("parse_julia.jl")
+# include("parse_julia.jl")
 include("dependencies.jl")
 include("ideals.jl")
 include("singular_imap.jl")
+include("looptransform.jl")
 
 
 function aligator(str::String)
     _, total = @timed begin
 
-        loop, time = @timed extract_loop(str)
+        loops, time = @timed extract_loops(Meta.parse(str))
+        vars = Base.unique(Iterators.flatten(map(Recurrences.free_symbols, loops)))
         @info "Recurrence extraction" time
+        @info loops vars
 
-        cforms, time = @timed closed_forms(loop)
-        @info "Recurrence solving" time
-        
-        invs, time = @timed invariants(cforms)
-        @info "Ideal computation" time
+        cforms = map(x -> Recurrences.solve(lrs_sequential(Vector{Expr}(x.args))), loops)
+        # cforms, time = @timed closed_forms(loop)
+        # @info "Recurrence solving" time
+        @info "" cforms
+        # @info "" map(polys, cforms)
+        invs, time = @timed invariants(cforms, vars)
+        @info "" invs
+        # @info "Ideal computation" time
     end
     @info "Total time needed" total
     
-    return invs
+    # return invs
+end
+
+function polys(cs::Vector{T}, vars::Vector{Symbol}) where {T<:Recurrences.ClosedForm}
+    ls = Basic[]
+    vs = Symbol[]
+    for c in cs
+        p = c.func - expression(c)
+        p = SymEngine.expand(p * denominator(p))
+        push!(ls, p)
+        push!(vs, Symbol(string(c.func)))
+    end
+    for v in setdiff(vars, vs)
+        push!(ls, :($v - $(Recurrences.initvariable(v, 0))))
+    end
+    ls
+end
+
+function invariants(cs::Vector{Vector{T}}, vars::Vector{Symbol}) where {T<:Recurrences.ClosedForm}
+    ps = [polys(c, vars) for c in cs]
 end
 
 function __init__()
---
+++ src/looptransform.jl
@@ -0,0 +1,36 @@
+function transform(expr::Expr)
+    expr = MacroTools.postwalk(x -> x isa Expr &&  x.head == :elseif ? Expr(:if, x.args...) : x, expr)
+    _transform(MacroTools.striplines(expr))
+end
+
+merge(x::Expr, ys::Vector{Expr}) = [merge(x, y) for y in ys]
+merge(xs::Vector{Expr}, y::Expr) = [merge(x, y) for x in xs]
+merge(xs::Vector{Expr}, ys::Vector{Expr}) = [merge(x, y) for x in xs for y in ys]
+merge(x) = x
+
+function merge(x::Expr, y::Expr)
+    @assert x.head == :block && y.head == :block
+    Expr(:block, x.args..., y.args...)
+end
+
+function _transform(expr)
+    res = @match expr begin
+        if _ x_ end         => [_transform(x), Expr(:block)]
+        if _ x_ else y_ end => [_transform(x); _transform(y)]
+        (x_ = y_)           => [block(expr)]
+    end
+    if res == nothing
+        if expr isa Expr && expr.head == :block
+            res = merge(reduce(merge, _transform.(expr.args)))
+        else
+            @error("Unsupported program construct: $expr")
+            res = Expr(:block)
+        end
+    end
+    return res
+end
+
+function extract_loops(expr::Expr)
+    @assert expr.head == :for || expr.head == :while "Not a loop"
+    transform(expr.args[2])
+end
