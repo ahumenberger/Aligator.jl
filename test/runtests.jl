95cb54ac2e01132f16e1fa04ea9af5d6e3f5ff85
Andreas Humenberger <ahumenberger@gmail.com>
2019-03-24 14:47:12 +0100

    Cleanup tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,2 @@
 using Aligator
-using Base.Test
-
-# write your own tests here
-include("testpetkovsek.jl")
+using Test
--- test/testpetkovsek.jl
+++
@@ -1,8 +0,0 @@
-using Petkovsek
-using SymPy
-
-@syms n y
-
-@test algpoly([3, n, n-1], 0*n, n) == 0
-
-@test alghyper([2*n*(n+1), -(n^2 +3*n-2), n-1], n) == [n+1, 2]
