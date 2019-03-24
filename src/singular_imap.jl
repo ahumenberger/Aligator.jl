dd6acb35e17690ccf8d451b0fa44eca6eac59339
Andreas Humenberger <ahumenberger@gmail.com>
2019-03-24 14:59:46 +0100

    Cleanup

--- src/singular_imap.jl
+++ src/singular_imap.jl
@@ -18,15 +18,3 @@ Base.length(ideal::Singular.sideal) = ngens(ideal)
 Base.iterate(ideal::Singular.sideal) = ngens(ideal) > 0 ? (ideal[1], 1) : nothing
 Base.iterate(ideal::Singular.sideal, state) = state < ngens(ideal) ? (ideal[state+1], state+1) : nothing
 Base.eltype(::Singular.sideal) = Singular.spoly
-
-import Base.==
-
-function ==(I::Singular.sideal, J::Singular.sideal)
-    # TODO: is this the right kind of equality?
-    for (a, b) in zip(I, J)
-        if a != b
-            return false
-        end
-    end
-    return true
-end
