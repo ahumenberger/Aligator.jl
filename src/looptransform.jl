2cf988e4641705e457129818fea687b56ae26496
Andreas Humenberger <ahumenberger@gmail.com>
2019-08-13 18:44:24 +0200

    Fix loop transform

--- src/looptransform.jl
+++ src/looptransform.jl
@@ -1,6 +1,16 @@
 function transform(expr::Expr)
     expr = MacroTools.postwalk(x->x isa Expr && x.head == :elseif ? Expr(:if, x.args...) : x, expr)
-    _transform(MacroTools.striplines(expr))
+    branches = _transform(MacroTools.striplines(expr))
+
+    vars = Base.unique(Iterators.flatten(map(Recurrences.free_symbols, branches)))
+    for b in branches
+        lhss, _ = Recurrences.split_assign(Vector{Expr}(b.args))
+        left = setdiff(vars, lhss)
+        for v in left
+            push!(b.args, :($v = $v))
+        end
+    end
+    branches
 end
 
 merge(x::Expr, ys::Vector{Expr}) = [merge(x, y) for y in ys]
@@ -19,7 +29,7 @@ function _transform(expr)
         if _ x_ else y_ end => [_transform(x); _transform(y)]
         (x_ = y_)           => [block(expr)]
     end
-    if res == nothing
+    if res === nothing
         if expr isa Expr && expr.head == :block
             res = merge(reduce(merge, _transform.(expr.args)))
         else
