3397c0246604b28ad7e78456fe57f3e4efb8c9dd
Andreas Humenberger <ahumenberger@gmail.com>
2020-10-12 09:54:54 +0200

    Add benchmarks

--- benchmark/multipath.jl
+++ benchmark/multipath.jl
@@ -103,3 +103,92 @@ divbin = quote
         end
     end
 end
+
+extpsolv2 = quote
+    n = 1
+    while true
+        if true
+            a = 2*(n+1)*(n+3/2)*a
+            b = 4*(n+1)*b
+            c = 1/2*(n+3/2)*c
+            n = n+1
+        else
+            a = 2*a
+            b = 4*b
+            c = 1/2*c
+        end
+    end
+end
+
+extpsolv3 = quote
+    n1, n2 = 1, 1
+    while true
+        if true
+            a = 2*(n1+1)*(n1+3/2)*a
+            b = 4*(n1+1)*b
+            c = 1/2*(n1+3/2)*c
+            n1 = n1+1
+        elseif c>0
+            a = 2*a
+            b = 4*b
+            c = 1/2*c
+        else
+            a = 2*(n2+1)^3*(n2+3/2)^3*a
+            b = 4*(n2+1)^3*b
+            c = 1/2*(n2+3/2)^3*c
+            n2 = n2+1
+        end
+    end
+end
+
+extpsolv10 = quote
+    n1, n2, n3, n4, n5 = 1, 1, 1, 1, 1
+    while true
+        if true
+            a = 2*(n1+1)*(n1+3/2)*a
+            b = 4*(n1+1)*b
+            c = 1/2*(n1+3/2)*c
+            n1 = n1+1
+        elseif c>0
+            a = 2*a
+            b = 4*b
+            c = 1/2*c
+        elseif c<0
+            a = 2*(n2+1)^3*(n2+3/2)^3*a
+            b = 4*(n2+1)^3*b
+            c = 1/2*(n2+3/2)^3*c
+            n2 = n2+1
+        elseif c<0
+            a = 6*a
+            b = 36*b
+            c = 1/6*c
+        elseif c<0
+            a = 2*(n3+1)^5*(n3+3/2)^5*a
+            b = 4*(n3+1)^5*b
+            c = 1/2*(n3+3/2)^5*c
+            n3 = n3+1
+        elseif c<0
+            a = 3*a
+            b = 9*b
+            c = 1/3*c
+        elseif c<0
+            a = 2*(n4+1)^7*(n4+3/2)^7*a
+            b = 4*(n4+1)^7*b
+            c = 1/2*(n4+3/2)^7*c
+            n4 = n4+1
+        elseif c<0
+            a = 5*a
+            b = 25*b
+            c = 1/5*c      
+        elseif c<0
+            a = 2*(n5+1)^9*(n5+3/2)^9*a
+            b = 4*(n5+1)^9*b
+            c = 1/2*(n5+3/2)^9*c
+            n5 = n5+1
+        else
+            a = 4*a
+            b = 16*b
+            c = 1/4*c
+        end
+    end
+end
